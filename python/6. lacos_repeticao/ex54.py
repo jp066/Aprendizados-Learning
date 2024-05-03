6344139b9b00effc5ced093b63670de0368d2f63
joao pedro matos <jpedro030506@gmail.com>
2024-05-02 21:54:53 -0300

    .

---
+++ python/6. lacos_repeticao/ex54.py
@@ -0,0 +1,20 @@
+import datetime
+# Crie um programa que leia o ano de nascimento de sete pessoas.
+# No final, mostre quantas pessoas ainda não atingiram a maioridade 
+#e quantas já são maiores.
+data_atual = datetime.date.today().year
+totalMaior = 0
+totalMenor = 0
+
+for a in range(1, 8):
+    ano_nascimento = int(input(f'qual o ano de nascimento da {a} pessoa: '))
+    idade = data_atual - ano_nascimento
+    if idade >= 18:
+        totalMaior += 1
+    else:
+        totalMenor += 1
+print(f'''no total há
+      {totalMaior} pessoas maiores de idade.
+      {totalMenor} pessoas menores de idade.''')
+
+        
